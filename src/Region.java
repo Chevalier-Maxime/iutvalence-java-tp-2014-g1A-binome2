5ffc7a1ab774fd1a886cb00624ec6a26ce7c005e
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:15:38 +0100

    TODO Javadoc (Region.java)

--- src/Region.java
+++ src/Region.java
@@ -5,18 +5,13 @@ public class Region
 	public final int nbArmee;
 	public Region[] voisins;
 	
-	public Region(String nom){
+	public Region(String nom) {
 		this.nom=nom;
 		this.nbArmee=0;
 	}
 	
-
-	
-	
 	public void ajouterVoisins(Region[] voisins) {
-		
-		this.voisins=voisins ;		// a verifier
-			
+		this.voisins=voisins;
 	}
 }
 
