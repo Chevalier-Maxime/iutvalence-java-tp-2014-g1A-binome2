480a3daa33e926641cad0d84a945a322874d3777
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:24:45 +0100

    TODO Javadoc (De.java)

--- src/De.java
+++ src/De.java
@@ -1,11 +1,10 @@
-
-public class De
-{
-	
-	
-	public static int lanceDe()
-	{
-		return (int)(1+ 6*Math.random());
-	}
-	
+/* TODO Package. */
+/* TODO JavaDoc. */
+/* TODO J'ai corrigé l'indentation, pensez à le faire. */
+public class De {
+    /* TODO Pourquoi mettre le mot "De" dans votre nom de méthode ? Nous sommes dans la classe "De"... */
+    public static int lanceDe() {
+        /* TODO On préférera SecureRandom à Random. */
+        return (int) (1 + (6 * Math.random()));
+    }
 }
