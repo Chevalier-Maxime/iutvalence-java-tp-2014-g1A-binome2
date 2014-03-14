35740050d454b2b5a397bb296bd98ca7031aa4f5
Anthony Gelibert <anthony.gelibert@me.com>
2014-03-14 11:25:34 +0100

    TODO Javadoc (Risk.java)

--- src/Risk.java
+++ src/Risk.java
@@ -1,12 +1,6 @@
-/* TODO Javadoc */
-public class Risk
-{
-	// http://jeuxstrategie.free.fr/Risk_complet.php#Texte%20des%20cartes%20objectifs:
+/* TODO Package. */
+/* TODO JavaDoc. */
+public class Risk {
+    // http://jeuxstrategie.free.fr/Risk_complet.php#Texte%20des%20cartes%20objectifs:
     /* TODO Vide ? */
-}	
-	
-	
-	
-
-	
-
+}
